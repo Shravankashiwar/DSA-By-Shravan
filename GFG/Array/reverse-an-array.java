378ae65e852ce57501fdf9850989f0a19851456b
Shravankashiwar <168186723+Shravankashiwar@users.noreply.github.com>
2026-07-18 17:20:52 +0530

    Create reverse-an-array.java

---
+++ GFG/Array/reverse-an-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void reverseArray(int arr[]) {
+        reverse(arr);
+        
+    }
+    public void reverse(int[] arr){
+        int i = 0;
+        int j = arr.length-1;
+        while(i < j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
