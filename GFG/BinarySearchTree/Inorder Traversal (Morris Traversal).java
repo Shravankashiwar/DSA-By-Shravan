78714f43fe7b4368596a72f5253c360bcc479852
Shravankashiwar <168186723+Shravankashiwar@users.noreply.github.com>
2026-07-29 16:24:35 +0530

    Update Inorder Traversal (Morris Traversal).java

--- GFG/BinarySearchTree/Inorder Traversal (Morris Traversal).java
+++ GFG/BinarySearchTree/Inorder Traversal (Morris Traversal).java
@@ -13,6 +13,7 @@ class Solution {
                     curr = curr.left;
                 }
                 else{
+                    pred.right = null;
                     ans.add(curr.data);
                     curr = curr.right;
                 }
