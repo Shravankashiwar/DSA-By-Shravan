fd55ca1a5d9286f2129039ae669bcd1ab72303ff
Shravankashiwar <168186723+Shravankashiwar@users.noreply.github.com>
2026-07-30 16:29:10 +0530

    Create Two Sum.java

---
+++ GFG/Maps & Sets/Two Sum.java
@@ -0,0 +1,11 @@
+class Solution {
+    boolean twoSum(int arr[], int target) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i<arr.length; i++){
+            int sum = target-arr[i];
+            if(set.contains(sum)) return true;
+            else set.add(arr[i]);
+        }
+        return false;
+    }
+}
