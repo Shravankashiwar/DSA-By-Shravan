99710f87855f35ca20089adee1cced3090973519
Shravankashiwar <168186723+Shravankashiwar@users.noreply.github.com>
2026-07-30 11:57:52 +0530

    Create Postorder Traversal (iterative).java

---
+++ LEETCODE/BinaryTree/Postorder Traversal (iterative).java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        if(root != null) st.push(root);
+        while(!st.isEmpty()){
+            TreeNode top = st.pop();
+            ans.add(top.val);
+            if(top.left != null) st.push(top.left);
+            if(top.right != null) st.push(top.right);
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
