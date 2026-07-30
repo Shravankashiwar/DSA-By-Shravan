5f02e6e9993eed970d56fb56fb4f23271fc62343
Shravankashiwar <168186723+Shravankashiwar@users.noreply.github.com>
2026-07-30 11:36:04 +0530

    Create Preorder Traversal (iterative).java

---
+++ LEETCODE/BinaryTree/Preorder Traversal (iterative).java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        if(root != null) st.push(root);
+        while(!st.isEmpty()){
+            TreeNode top = st.pop();
+            ans.add(top.val);
+            if(top.right != null) st.push(top.right);
+            if(top.left != null) st.push(top.left);
+        }
+        return ans;
+    }
+}
