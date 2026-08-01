576aae8d87e94e048ae5ec004cbc9a3af9dec36a
Shravankashiwar <168186723+Shravankashiwar@users.noreply.github.com>
2026-08-01 14:01:39 +0530

    Create Longest Substring with Distinct Characters.java

---
+++ GFG/Maps & Sets/Longest Substring with Distinct Characters.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestUniqueSubstr(String s) {
+        int maxLen = 0;
+        int i = 0;
+        int j = 0;
+        HashSet<Character> set = new HashSet<>();
+        while(j < s.length()){
+            if(!set.contains(s.charAt(j))){
+                set.add(s.charAt(j));
+                j++;
+            }
+            else{
+                int len = j-i;
+                maxLen = Math.max(len,maxLen);
+                while(s.charAt(i) != s.charAt(j)){
+                    set.remove(s.charAt(i));
+                    i++;
+                }
+                i++;
+                j++;
+            }
+        }
+        int len = j-i;
+        maxLen = Math.max(len,maxLen);
+        return maxLen;
+    }
+}
