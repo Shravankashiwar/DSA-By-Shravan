4d902324d95ffe395662de8af66df1851fcf2f6c
Shravankashiwar <168186723+Shravankashiwar@users.noreply.github.com>
2026-08-06 19:15:36 +0530

    Create Stickler Thief (Recursive).java

---
+++ GFG/DP/Stickler Thief (Recursive).java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findMaxSum(int arr[]) {
+        return loot(0,arr);
+    }
+    public int loot(int i, int[] arr){
+        if(i >= arr.length) return 0;
+        int pick = arr[i] + loot(i+2,arr);
+        int skip = loot(i+1,arr);
+        return Math.max(pick,skip);
+    }
+}
