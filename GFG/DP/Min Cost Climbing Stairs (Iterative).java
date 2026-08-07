35356dfe99458233621d600a73160426c342570e
Shravankashiwar <168186723+Shravankashiwar@users.noreply.github.com>
2026-08-07 17:37:29 +0530

    Create Min Cost Climbing Stairs (Iterative).java

---
+++ GFG/DP/Min Cost Climbing Stairs (Iterative).java
@@ -0,0 +1,14 @@
+class Solution {
+    static int minCostClimbingStairs(int[] cost) {
+        int[] dp =  new int[cost.length];
+        Arrays.fill(dp,-1);
+        return Math.min(climb(0,cost,dp),climb(1,cost,dp));
+    }
+    static int climb(int i, int[] arr, int[] dp){
+        if(i >= arr.length) return 0;
+        if(dp[i] != -1) return dp[i];
+        int ans = arr[i] + Math.min(climb(i+1,arr,dp),climb(i+2,arr,dp));
+        dp[i] = ans;
+        return ans;
+    }
+};
