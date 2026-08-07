abec196d3b0e82f5f1f64526d9119aee79c73cb0
Shravankashiwar <168186723+Shravankashiwar@users.noreply.github.com>
2026-08-07 17:32:01 +0530

    Create Min Cost Climbing Stairs (Recursive).java

---
+++ LEETCODE/DP/Min Cost Climbing Stairs (Recursive).java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        return Math.min(climb(0,cost),climb(1,cost));
+    }
+    static int climb(int i, int[] arr){
+        if(i >= arr.length) return 0;
+        int ans = arr[i] + Math.min(climb(i+1,arr),climb(i+2,arr));
+        return ans;
+    }
+}
