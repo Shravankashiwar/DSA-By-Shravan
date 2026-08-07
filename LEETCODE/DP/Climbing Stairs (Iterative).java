249e39480b16be2f50b736e00874b456cec16ae0
Shravankashiwar <168186723+Shravankashiwar@users.noreply.github.com>
2026-08-07 18:54:41 +0530

    Create Climbing Stairs (Iterative).java

---
+++ LEETCODE/DP/Climbing Stairs (Iterative).java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return count(n,dp);
+    }
+    public int count(int n, int[] dp) {
+        if(n <= 1) return 1;
+        if(dp[n] != -1) return dp[n];
+        int ans = count(n-1,dp) + count(n-2,dp);
+        dp[n] = ans;
+        return ans;
+    }
+}
