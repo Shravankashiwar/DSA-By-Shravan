27908b879de6ee7524da29bee6327ab187883204
Shravankashiwar <168186723+Shravankashiwar@users.noreply.github.com>
2026-09-20 19:46:36 +0530

    Update First and Last in Sorted.java

--- GFG/BinarySearch/First and Last in Sorted.java
+++ GFG/BinarySearch/First and Last in Sorted.java
@@ -1,40 +1,41 @@
 class Solution {
-    ArrayList<Integer> find(int arr[], int tar) {
-        ArrayList<Integer> ans = new ArrayList<>();
+    ArrayList<Integer> find(int arr[], int target) {
         int n = arr.length;
-        int first_occur = first(0, n-1, arr, tar);
-        int last_occur = last(0, n-1, arr, tar);
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        int first_occur = first(0, n-1, arr, target);
+        int last_occur = last(0, n-1, arr, target);
+        
         ans.add(first_occur);
         ans.add(last_occur);
+        
         return ans;
         
     }
-    int first(int lo, int hi, int[] arr, int tar){
-        
+    static int first(int lo, int hi, int[] arr, int target){
+        int idx = -1;
         while(lo <= hi){
             int mid = (lo+hi)/2;
-            if(arr[mid] > tar) hi = mid - 1;
-            else if(arr[mid] < tar) lo = mid + 1;
-            else{
-                if(mid == 0 || arr[mid-1] != tar) return mid;
-                else hi = mid - 1;
+            if(arr[mid] == target){
+                hi = mid - 1;
+                idx = mid;
             }
+            else if(arr[mid] > target) hi = mid - 1;
+            else lo = mid + 1;
         }
-        return -1;
-        
+        return idx;
     }
-    int last(int lo, int hi, int[] arr, int tar){
-        
+    static int last(int lo, int hi, int[] arr, int target){
+        int idx = -1;
         while(lo <= hi){
             int mid = (lo+hi)/2;
-            if(arr[mid] > tar) hi = mid - 1;
-            else if(arr[mid] < tar) lo = mid + 1;
-            else{
-                if(mid == arr.length-1 || arr[mid+1] != tar) return mid;
-                else lo = mid +1;
+            if(arr[mid] == target){
+                lo = mid + 1;
+                idx = mid;
             }
+            else if(arr[mid] > target) hi = mid - 1;
+            else lo = mid + 1;
         }
-        return -1;
-        
+        return idx;
     }
 }
