808f16d2796c5156313c8bd70caf28e81f3a6fc7
Shravankashiwar <168186723+Shravankashiwar@users.noreply.github.com>
2026-09-20 19:52:30 +0530

    Refactor searchRange method and update variable names

--- LEETCODE/BinarySearch/Find First and Last Position of Element in Sorted Array.java
+++ LEETCODE/BinarySearch/Find First and Last Position of Element in Sorted Array.java
@@ -1,39 +1,42 @@
 class Solution {
-    public int[] searchRange(int[] arr, int tar) {
-        int[] ans = new int[2];
+    public int[] searchRange(int[] arr, int target) {
         int n = arr.length;
-        int first_occur = first(0, n-1, arr, tar);
-        int last_occur = last(0, n-1, arr, tar);
+        
+        int[] ans = new int[2];
+        
+        int first_occur = first(0, n-1, arr, target);
+        int last_occur = last(0, n-1, arr, target);
+        
         ans[0] = first_occur;
         ans[1] = last_occur;
+        
         return ans;
         
     }
-    int first(int lo, int hi, int[] arr, int tar){
-        
+    static int first(int lo, int hi, int[] arr, int target){
+        int idx = -1;
         while(lo <= hi){
             int mid = (lo+hi)/2;
-            if(arr[mid] > tar) hi = mid - 1;
-            else if(arr[mid] < tar) lo = mid + 1;
-            else{
-                if(mid == 0 || arr[mid-1] != tar) return mid;
-                else hi = mid - 1;
+            if(arr[mid] == target){
+                hi = mid - 1;
+                idx = mid;
             }
+            else if(arr[mid] > target) hi = mid - 1;
+            else lo = mid + 1;
         }
-        return -1;
-        
+        return idx;
     }
-    int last(int lo, int hi, int[] arr, int tar){
-        
+    static int last(int lo, int hi, int[] arr, int target){
+        int idx = -1;
         while(lo <= hi){
             int mid = (lo+hi)/2;
-            if(arr[mid] > tar) hi = mid - 1;
-            else if(arr[mid] < tar) lo = mid + 1;
-            else{
-                if(mid == arr.length-1 || arr[mid+1] != tar) return mid;
-                else lo = mid +1;
+            if(arr[mid] == target){
+                lo = mid + 1;
+                idx = mid;
             }
+            else if(arr[mid] > target) hi = mid - 1;
+            else lo = mid + 1;
         }
-        return -1;
+        return idx;
     }
 }
